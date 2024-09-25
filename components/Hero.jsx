96f89877be194086a5181964ad187d8905bd80ce
Joseph Kiarie <microstation.k@gmail.com>
2024-09-25 20:13:11 +0300

    changed color

--- components/Hero.jsx
+++ components/Hero.jsx
@@ -5,8 +5,8 @@ import Link from 'next/link';
 
 const Hero = () => {
 	return (
-		<section className="relative bg-blue-600 text-white p-10 rounded-lg mb-10">
-			<div className="absolute inset-0 bg-gradient-to-r from-blue-600 via-blue-500 to-blue-400 opacity-90 rounded-lg"></div>
+		<section className="relative bg-[#cca6c8] text-white p-10 rounded-lg mb-10">
+			<div className="absolute inset-0 bg-gradient-to-r from-[#cca6c8] via-[#cca6c8]to-[#cca6c8] opacity-90 rounded-lg"></div>
 			<motion.div
 				className="relative z-10 flex flex-col items-center"
 				initial={{ opacity: 0, y: -50 }}
@@ -24,7 +24,7 @@ const Hero = () => {
 					care for your loved ones.
 				</p>
 				<Link
-					className="bg-white text-blue-600 py-3 px-8 rounded-lg font-semibold hover:bg-blue-700 hover:text-white transition-all duration-300"
+					className="bg-white text-blue-800 py-3 px-8 rounded-lg font-semibold hover:bg-blue-800 hover:text-white transition-all duration-300"
 					href="/services">
 					Learn More
 				</Link>
